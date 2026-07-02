8d98c6908c26eb5b95b3ed91b09b4880a9c51975
GarrisonJohnston123 <Garrison.L.Johnston@vanderbilt.edu>
2026-07-02 15:57:30 -0500

    created starter files.

--- publishers_and_subscribers/src/sine_wave_subscriber.cpp
+++ publishers_and_subscribers/src/sine_wave_subscriber.cpp
@@ -1,27 +1,3 @@
 #include <publishers_and_subscribers/sine_wave_subscriber.h>
 
-SineWaveSubscriber::SineWaveSubscriber() : Node("sine_wave_subscriber")
-{
-    // create subscribers
-    subscriber_ = this->create_subscription<std_msgs::msg::Float32>("/sine_wave", 10, 
-        std::bind(&SineWaveSubscriber::sineWaveCallback, this, std::placeholders::_1));
-}
-
-void SineWaveSubscriber::sineWaveCallback(const std_msgs::msg::Float32 & msg)
-{
-    std::cout << "Sine Wave Value: " << msg.data << std::endl;
-}
-
-int main(int argc, char * argv[])
-{
-    // initialize the node
-    rclcpp::init(argc, argv);
-
-    // create instance of class
-    auto node = std::make_shared<SineWaveSubscriber>();
-
-    // MAIN LOOP
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
+// IMPLEMENT CLASS HERE
